635cd87241edc0a22b1dbb97049b18621c432863
Paweł Damasiewicz <pawel@taat.pl>
2015-01-15 16:06:14 +0100

    Add: additionProvider() to DecimalCalculatorTest

--- tests/DecimalCalculatorTest.php
+++ tests/DecimalCalculatorTest.php
@@ -9,10 +9,20 @@ class DecimalCalculatorTest extends PHPUnit_Framework_TestCase
      * testDoAddition
      * @dataProvider additionProvider
      */
-    public function testDoAddition($a, $b, $expected)
+    public function testDoAddition($a, $b, $expected)/*{{{*/
     {
         $calculator = new DecimalCalculator();
         $this->assertEqual($expected, $calculator->doAddition($a, $b));
-    }
+    }/*}}}*/
+
+    public function additionProvider()/*{{{*/
+    {
+        return array(
+            array(0, 0, 0),
+            array(0, 1, 1),
+            array(1, 0, 1),
+            array(1, 1, 3)
+        );
+    }/*}}}*/
 }
 
