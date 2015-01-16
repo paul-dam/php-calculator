12d07f80f10866afbc0688bceebde2bae422a50a
Paweł Damasiewicz <pawel@taat.pl>
2015-01-16 10:37:10 +0100

    Change: implement doSubstraction in Calculator

--- src/Calculator.php
+++ src/Calculator.php
@@ -11,11 +11,15 @@ abstract class Calculator/*{{{*/
     protected $_multiplicationBehavior;
     protected $_divisionBehavior;
 
-    public abstract function doSubstraction($a, $b);
     public abstract function doMultiplication($a, $b);
     public abstract function doDivision($a, $b);
     public function doAddition($a, $b)/*{{{*/
     {
         return $this->_additionBehavior->add($a, $b);
+    }/*}}}*/
+
+    public function doSubstraction($a, $b)/*{{{*/
+    {
+        return $this->$_substractionBehavior->substract($a, $b);
     }/*}}}*/
 }/*}}}*/
