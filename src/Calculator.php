683341b72d1343248c2a2129a2048be02ceddc5d
Paweł Damasiewicz <pawel@taat.pl>
2015-01-16 10:33:46 +0100

    Add: doAddition to Calculator

--- src/Calculator.php
+++ src/Calculator.php
@@ -2,13 +2,20 @@
 
 /**
  * @SuppressWarnings(PHPMD.ShortVariable)
+ * @SuppressWarnings(PHPMD.LongVariable)
  */
 abstract class Calculator/*{{{*/
 {
     protected $_additionBehavior;
+    protected $_substractionBehavior;
+    protected $_multiplicationBehavior;
+    protected $_divisionBehavior;
 
-    public abstract function doAddition($a, $b);
     public abstract function doSubstraction($a, $b);
     public abstract function doMultiplication($a, $b);
     public abstract function doDivision($a, $b);
+    public function doAddition($a, $b)/*{{{*/
+    {
+        return $this->_additionBehavior->add($a, $b);
+    }/*}}}*/
 }/*}}}*/
