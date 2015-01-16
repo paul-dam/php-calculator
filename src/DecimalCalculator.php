ce4ecb5f1fad4fe26157f4327a1eeef3500e15fa
Paweł Damasiewicz <pawel@taat.pl>
2015-01-16 10:35:14 +0100

    Remove: _additionInterface from DecimalCalculator

--- src/DecimalCalculator.php
+++ src/DecimalCalculator.php
@@ -2,8 +2,6 @@
 
 class DecimalCalculator extends Calculator
 {
-    private $_additionInterface;
-
     public function __construct() 
     {
         $this->_additionBehavior = new DecimalAdditionBehavior();
