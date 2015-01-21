f8f0a0a146134346a8bdc3ebb4477ffa84e143a6
Paweł Damasiewicz <pawel@taat.pl>
2015-01-21 09:40:16 +0100

    Add: DivisionBehavior interface

---
+++ src/DivisionBehavior.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @SuppressWarnings(PHPMD.ShortVariable)
+ * @SuppressWarnings(PHPMD.LongVariable)
+ * @SuppressWarnings(PHPMD.UnusedLocalVariable)
+ */
+interface DivisionBehavior
+{
+    public function doDivision($a, $b);
+}
+
