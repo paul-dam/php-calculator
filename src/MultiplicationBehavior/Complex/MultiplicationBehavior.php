0c341ec5680e77a517afdc469570fd81a4f247bd
Paweł Damasiewicz <pawel@taat.pl>
2015-02-19 15:11:15 +0100

    Add Complex template calculator behaviors

---
+++ src/MultiplicationBehavior/Complex/MultiplicationBehavior.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Complex MultiplicationBehavior
+ */
+class ComplexMultiplicationBehavior implements MultiplicationBehavior
+{
+    public function execute($left, $right)
+    {
+        throw new Exception('Not implemented');
+    }
+}
