fff03818424db2583fbb77c261b932b158119288
Paweł Damasiewicz <pawel.damasiewicz@gmail.com>
2015-02-20 23:44:46 +0000

    Add Complex\DivisionBehavior#execute method

--- src/DivisionBehavior/Complex/DivisionBehavior.php
+++ src/DivisionBehavior/Complex/DivisionBehavior.php
@@ -4,6 +4,14 @@ class ComplexDivisionBehavior implements DivisionBehavior
 {
     public function execute($left, $right)
     {
-        throw new Exception('Not implemented');
+        $result = new Complex();
+        if (0 == $right->getRe() && 0 == $right->getIm()) {
+            throw new InvalidArgumentException('At least one of the real part and the imaginary part of the denominator must be nonzero for division to be defined.');
+        }
+
+        $result->setRe((($left->getRe() * $right->getRe()) + ($left->getIm() * $right->getIm()))/(pow($right->getRe(), 2) + pow($right->getIm(), 2)));
+        $result->setIm((($left->getIm() * $right->getRe()) - ($left->getRe() * $right->getIm()))/(pow($right->getRe(), 2) + pow($right->getIm(), 2)));
+
+        return($result);
     }
 }
