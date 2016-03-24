bc49a3641514673cb8d2b471e0c6c66d72e4c4e2
Paweł Damasiewicz <pawel.damasiewicz@performgroup.com>
2016-03-24 18:49:34 +0100

    change: Complex subxtraction to extend Complex addition

--- src/Behaviors/Substraction/Complex.php
+++ src/Behaviors/Substraction/Complex.php
@@ -1,16 +1,16 @@
 <?php namespace Pdam\Behaviors\Substraction;
 
 use Pdam\Behaviors\Substraction;
+use Pdam\Behaviors\Addition\Complex as ComplexAdditionBehavior;
 use Pdam\Struct\Complex as StructComplex;
 
-class Complex implements Substraction
+class Complex extends ComplexAdditionBehavior implements Substraction
 {
     public function execute($left, $right)
     {
-        $result = new StructComplex();
-        $result->setRe($left->getRe() - $right->getRe());
-        $result->setIm($left->getIm() - $right->getIm());
+        $right->setRe(0 - $right->getRe());
+        $right->setIm(0 - $right->getIm());
 
-        return($result);
+        return parent::execute($left, $right);
     }
 }
