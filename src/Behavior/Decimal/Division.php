5b1d74a7864cd0bf2655df7441ec3fefc4b4c534
Pawel Damasiewicz <pawel.damasiewicz@performgroup.com>
2017-06-08 16:39:36 +0200

    modify: Decimal\Division - remove redundant new lines

--- src/Behavior/Decimal/Division.php
+++ src/Behavior/Decimal/Division.php
@@ -7,9 +7,7 @@ class Division implements DivisionInterface
     public function execute($numerator, $denominator)
     {
         if (0 === $denominator) {
-            throw new \InvalidArgumentException(
-                'Denominator cannot be `0`';
-            );
+            throw new \InvalidArgumentException('Denominator cannot be `0`');
         }
 
         return $numerator / $denominator;
